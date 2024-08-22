0cd876f979c4bd54b99b82069d18f2ccc263f339
rchrdwork <richard@momentums.dev>
2024-08-21 23:56:00 -0400

    proveedores, paginacion, tabla

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,10 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.InventoryHomeView.as_view(), name='inventory-home'),
     path('productos/', views.InventoryProduct.as_view(), name='producto-list-create'),
-    path('api_productos/', views.ProductoListCreate.as_view(), name='producto-list-view'),
-    path('productos/<int:pk>/', views.ProductoDetailUpdateDelete.as_view(), name='producto-detail-update-delete'),
-    path('categorias/', views.CategoriaListCreate.as_view(), name='categoria-list-create'),
     path('proveedores/', views.InventoryProviders.as_view(), name='proveedor-list-create'),
-    path('api_proveedores/', views.ProveedorListCreate.as_view(), name='proveedor-list-view'),
-    path('productos/<int:pk>/', views.ProductoDetalleView.as_view(), name='producto-detalle'),
+    path('api/proveedores/', views.ProveedoresAPIView.as_view(), name='proveedores-api'),
+    path('proveedores/create/', views.CreateProveedorView.as_view(), name='proveedor-create'),
+
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,44 +1,53 @@
-from rest_framework import generics
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.generic.edit import CreateView
+
+from rest_framework import generics, viewsets, serializers, filters
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
+
 from .models import Producto, Categoria, Proveedor
 from .serializers import ProductoSerializer, CategoriaSerializer, ProveedorSerializer
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.views import View
-from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
 
-class ProductoListCreate(generics.ListCreateAPIView):
-    queryset = Producto.objects.all()
-    serializer_class = ProductoSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+class ProveedorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proveedor
+        fields = '__all__'
 
-class ProductoDetailUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Producto.objects.all()
-    serializer_class = ProductoSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+class ProveedorPagination(PageNumberPagination):
+    page_size = 10  
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
-class CategoriaListCreate(generics.ListCreateAPIView):
-    queryset = Categoria.objects.all()
-    serializer_class = CategoriaSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-class ProveedorListCreate(generics.ListCreateAPIView):
+class ProveedorViewSet(viewsets.ModelViewSet):
     queryset = Proveedor.objects.all()
     serializer_class = ProveedorSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+    pagination_class = ProveedorPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'correo_electronico', 'telefono']
     
-class ProductoDetalleView(generics.RetrieveAPIView):
-    queryset = Producto.objects.all()
-    serializer_class = ProductoSerializer
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
+
+class CreateProveedorView(CreateView):
+    model = Proveedor
+    fields = ['nombre', 'informacion_contacto', 'sitio_web', 'correo_electronico', 'telefono']
+    success_url = reverse_lazy('proveedor-list-create')
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return JsonResponse({'status': 'success', 'message': 'Proveedor guardado correctamente'})
+
+    def form_invalid(self, form):
+        return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
+
+
+
 
 
 
@@ -81,17 +90,18 @@ class InventoryProduct(View):
 
 class InventoryProviders(View):
     template_name = 'inventory/providers.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class ProveedoresAPIView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    @method_decorator(login_required)  
     def get(self, request, *args, **kwargs):
-        context = {
-            'welcome_message': 'Bienvenido a StockMaster',
-        
-        }
-        return render(request, self.template_name, context)
-
-    @method_decorator(login_required)
-    def post(self, request, *args, **kwargs):
-        pass
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+        proveedores = Proveedor.objects.all()
+        result_page = paginator.paginate_queryset(proveedores, request)
+        serializer = ProveedorSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
