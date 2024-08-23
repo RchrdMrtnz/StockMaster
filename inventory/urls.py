563155c2d0d1c2b30ed3cef179f3cac17d61e54b
rchrdwork <richard@momentums.dev>
2024-08-22 21:31:34 -0400

    listar productos

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,12 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.InventoryHomeView.as_view(), name='inventory-home'),
     path('productos/', views.InventoryProduct.as_view(), name='producto-list-create'),
-    path('api/productos/', views.ProductoCreateAPIView.as_view(), name='producto-create'), 
-    path('api/productos/create/', views.ProductoCreateAPIView.as_view(), name='producto-create'),
+    
+    # API de Productos
+    path('api/productos/', views.ProductoListCreateAPIView.as_view(), name='productos-api'),
+    path('api/productos/<int:product_id>/agregar_stock/', views.AgregarStockAPIView.as_view(), name='agregar-stock'),
+    path('api/productos/<int:product_id>/descontar_stock/', views.DescontarStockAPIView.as_view(), name='descontar-stock'),
+    path('api/productos/<int:product_id>/delete/', views.ProductoDetailDeleteAPIView.as_view(), name='producto-detail-delete'),
     
     # Proveedores
     path('proveedores/', views.InventoryProviders.as_view(), name='proveedor-list-create'),
     path('api/proveedores/', views.ProveedoresAPIView.as_view(), name='proveedores-api'),
     path('proveedores/create/', views.CreateProveedorView.as_view(), name='proveedor-create'),
-    path('api/proveedores/<int:pk>/', views.ProveedoresAPIView.as_view(), name='proveedor-delete'),
+    path('api/proveedores/<int:pk>/delete/', views.ProveedoresAPIView.as_view(), name='proveedor-delete'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -15,32 +15,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Producto, Categoria, Proveedor,DetalleProducto
+from .models import Producto, Categoria, Proveedor, DetalleProducto
 from .serializers import ProductoSerializer, CategoriaSerializer, ProveedorSerializer
 
+#############
+# Proveedores
+#############
 
-class ProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields = ['nombre', 'sku', 'categoria', 'cantidad', 'proveedores']
-        
-class ProveedorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Proveedor
-        fields = '__all__'
 
 class ProveedorPagination(PageNumberPagination):
     page_size = 10  
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-class ProveedorViewSet(viewsets.ModelViewSet):
-    queryset = Proveedor.objects.all()
-    serializer_class = ProveedorSerializer
-    pagination_class = ProveedorPagination
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['nombre', 'correo_electronico', 'telefono']
-    
 
 class CreateProveedorView(CreateView):
     model = Proveedor
@@ -54,90 +41,6 @@ class CreateProveedorView(CreateView):
     def form_invalid(self, form):
         return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
 
-
-
-
-
-
-class InventoryHomeView(View):
-    template_name = 'inventory/home.html'
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    @method_decorator(login_required)  # Asegura que solo usuarios autenticados puedan acceder
-    def get(self, request, *args, **kwargs):
-        context = {
-            'welcome_message': 'Bienvenido a StockMaster',
-            # Aquí puedes agregar más contexto o lógica para la vista
-        }
-        return render(request, self.template_name, context)
-
-    @method_decorator(login_required)
-    def post(self, request, *args, **kwargs):
-        # Aquí puedes manejar la lógica de un formulario o cualquier acción de post
-        pass
-    
-
-class InventoryProduct(View):
-    template_name = 'inventory/product.html'
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        categorias = Categoria.objects.all()
-        proveedores = Proveedor.objects.all()
-        context = {
-            'categorias': categorias,
-            'proveedores': proveedores,
-        }
-        return render(request, self.template_name, context)
-    
-class ProductoCreateAPIView(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, *args, **kwargs):
-        data = request.data
-
-        # Crear el producto
-        producto = Producto.objects.create(
-            nombre=data['nombre'],
-            sku=data['sku'],
-            categoria_id=data['categoria'],
-            cantidad=data['cantidad']
-        )
-
-        # Asignar los proveedores seleccionados
-        proveedores = data.getlist('proveedores')
-        if proveedores:
-            producto.proveedores.set(proveedores)
-
-        # Crear el detalle del producto
-        DetalleProducto.objects.create(
-            producto=producto,
-            descripcion=data.get('descripcion', ''),
-            precio=data['precio'],
-            peso=data.get('peso', None),
-            longitud=data.get('longitud', None),
-            anchura=data.get('anchura', None),
-            altura=data.get('altura', None),
-            color=data.get('color', ''),
-            material=data.get('material', ''),
-            garantia=data.get('garantia', ''),
-            otras_especificaciones=data.get('otras_especificaciones', '')
-        )
-
-        return Response({'status': 'success', 'message': 'Producto creado correctamente'}, status=status.HTTP_201_CREATED)
-
-
-
-class InventoryProviders(View):
-    template_name = 'inventory/providers.html'
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
 
 class ProveedoresAPIView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -168,3 +71,145 @@ class ProveedoresAPIView(APIView):
             return Response({'status': 'success', 'message': 'Proveedor eliminado correctamente'}, status=status.HTTP_204_NO_CONTENT)
         except Proveedor.DoesNotExist:
             return Response({'status': 'error', 'message': 'Proveedor no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class InventoryProviders(View):
+    template_name = 'inventory/providers.html'
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+#############
+# Productos
+#############
+
+class ProductoListCreateAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        paginator = PageNumberPagination()
+        paginator.page_size = 10  
+        
+        search_term = request.query_params.get('search', '')
+
+        if search_term:
+            productos = Producto.objects.filter(
+                nombre__icontains=search_term
+            )
+        else:
+            productos = Producto.objects.all()
+
+        result_page = paginator.paginate_queryset(productos, request)
+        serializer = ProductoSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        producto = Producto.objects.create(
+            nombre=data['nombre'],
+            sku=data['sku'],
+            categoria_id=data['categoria'],
+            cantidad=data['cantidad']
+        )
+
+        proveedores = data.getlist('proveedores')
+        if proveedores:
+            producto.proveedores.set(proveedores)
+
+        DetalleProducto.objects.create(
+            producto=producto,
+            descripcion=data.get('descripcion', ''),
+            precio=data['precio'],
+            peso=data.get('peso', None),
+            longitud=data.get('longitud', None),
+            anchura=data.get('anchura', None),
+            altura=data.get('altura', None),
+            color=data.get('color', ''),
+            material=data.get('material', ''),
+            garantia=data.get('garantia', ''),
+            otras_especificaciones=data.get('otras_especificaciones', '')
+        )
+
+        return Response({'status': 'success', 'message': 'Producto creado correctamente'}, status=status.HTTP_201_CREATED)
+
+class AgregarStockAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, product_id):
+        try:
+            producto = Producto.objects.get(id=product_id)
+            cantidad = request.data.get('cantidad', 0)
+            producto.cantidad += int(cantidad)
+            producto.save()
+            return Response({'status': 'success', 'message': 'Stock agregado correctamente'}, status=status.HTTP_200_OK)
+        except Producto.DoesNotExist:
+            return Response({'status': 'error', 'message': 'Producto no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+class DescontarStockAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, product_id):
+        try:
+            producto = Producto.objects.get(id=product_id)
+            cantidad = request.data.get('cantidad', 0)
+            if producto.cantidad >= int(cantidad):
+                producto.cantidad -= int(cantidad)
+                producto.save()
+                return Response({'status': 'success', 'message': 'Stock descontado correctamente'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'status': 'error', 'message': 'Cantidad insuficiente'}, status=status.HTTP_400_BAD_REQUEST)
+        except Producto.DoesNotExist:
+            return Response({'status': 'error', 'message': 'Producto no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class ProductoDetailDeleteAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, product_id):
+        try:
+            producto = Producto.objects.get(id=product_id)
+            producto.delete()
+            return Response({'status': 'success', 'message': 'Producto eliminado correctamente'}, status=status.HTTP_204_NO_CONTENT)
+        except Producto.DoesNotExist:
+            return Response({'status': 'error', 'message': 'Producto no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+class InventoryProduct(View):
+    template_name = 'inventory/product.html'
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        categorias = Categoria.objects.all()
+        proveedores = Proveedor.objects.all()
+        context = {
+            'categorias': categorias,
+            'proveedores': proveedores,
+        }
+        return render(request, self.template_name, context)
+
+
+#############
+# Vistas generales
+#############
+class InventoryHomeView(View):
+    template_name = 'inventory/home.html'
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    @method_decorator(login_required)  
+    def get(self, request, *args, **kwargs):
+        context = {
+            'welcome_message': 'Bienvenido a StockMaster',
+        }
+        return render(request, self.template_name, context)
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        pass
